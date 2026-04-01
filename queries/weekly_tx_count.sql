2e6fac1ce30ae3f41fedcba7d0f6873ca8957e7c
akierzkowski-24 <akierzkowski24@gmail.com>
2026-04-01 13:42:03 +0200

    Create weekly_tx_count.sql for transaction aggregation
    Add SQL query to calculate weekly transaction count.

---
+++ queries/weekly_tx_count.sql
@@ -0,0 +1,11 @@
+WITH all_x402 AS (
+    SELECT * 
+    FROM dune.kundeus.result_raw_x402_historical
+    UNION ALL
+    SELECT * 
+    FROM dune.kundeus.result_x_402_updating
+)
+
+SELECT SUM(tx_count) AS weekly_tx_count
+FROM all_x402
+WHERE week = DATE_TRUNC('week', NOW()) - INTERVAL '7' DAY
